fda22122d3d37944ea7e32b0453a43611a4abc7a
Othniel Powell <othniel@live.ca>
2024-11-23 21:15:10 -0500

    added form and styles

---
+++ loginScript.js
@@ -0,0 +1,16 @@
+//Name this loginScript.js
+
+function userCheck(event) {
+    event.preventDefault
+    const username = document.getElementById('username').value; //gets the username
+    const password = document.getElementById('password').value; //gets the psas
+
+ if (username === 'admin' && password === '1234') { //if the username and password are these, you can enter
+        window.location.assign('index.html'); 
+        alert("Welcome!")
+ } else {
+    alert("Invalid information.");
+    return;
+ }
+
+}
